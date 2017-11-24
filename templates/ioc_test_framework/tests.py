851ca05d8d15c7f4a8181bc5cbd417f6f26d1fc4
Adrian Potter <Adrian.Potter@tessella.com>
2017-11-24 09:57:33 +0000

    Remove TEMP_TOLERANCE. Not needed by template

--- templates/ioc_test_framework/tests.py
+++ templates/ioc_test_framework/tests.py
@@ -10,9 +10,6 @@ class _Device_Tests(unittest.TestCase):
     """
     Tests for the _Device_ IOC.
     """
-
-    TEMP_TOLERANCE = 0.005
-
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("_device_")
         self.ca = ChannelAccess(device_prefix="_DEVICE__01")
