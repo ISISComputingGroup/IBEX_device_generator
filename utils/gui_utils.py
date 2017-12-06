14f05ebb38139058498e2023706bf4a8971f85f6
Adrian Potter <Adrian.Potter@tessella.com>
2017-12-06 15:16:43 +0000

    Add a default macro to opi_info.xml corresponding to PV prefix in OPI

--- utils/gui_utils.py
+++ utils/gui_utils.py
@@ -8,7 +8,7 @@ from lxml import etree
 import logging
 
 
-def _generate_opi_entry(opi_key, opi_file_name, descriptive_device_name):
+def _generate_opi_entry(opi_key, opi_file_name, descriptive_device_name, macro_name):
     """
     Generates an ElementTree entry for opi info based on a device name
     
@@ -16,6 +16,7 @@ def _generate_opi_entry(opi_key, opi_file_name, descriptive_device_name):
         opi_key: Key to identify to OPI to the GUI
         opi_file_name: File name to the device's opi
         descriptive_device_name: Human readable device name
+        macro_name: Name of the macro that is used to construct IOC PV prefix
     
     Returns:
         ElementTree template based on the device name
@@ -40,7 +41,16 @@ def _generate_opi_entry(opi_key, opi_file_name, descriptive_device_name):
     description.text = "The OPI for the {}".format(descriptive_device_name)
     value.append(description)
 
-    value.append(etree.Element("macros"))
+    macros = etree.Element("macros")
+    macro = etree.Element("macro")
+    name = etree.Element("name")
+    name.text = macro_name
+    description = etree.Element("description")
+    description.text = "The {} PV prefix (e.g. {}_01)".format(descriptive_device_name, macro_name)
+    macro.append(name)
+    macro.append(description)
+    macros.append(macro)
+    value.append(macros)
 
     entry.append(key)
     entry.append(value)
@@ -48,7 +58,7 @@ def _generate_opi_entry(opi_key, opi_file_name, descriptive_device_name):
     return entry
 
 
-def _update_opi_info(opi_key, opi_file_name, descriptive_device_name):
+def _update_opi_info(opi_key, opi_file_name, descriptive_device_name, device_macro_name):
     """
     Add some basic template information to the opi_info.xml file
 
@@ -56,6 +66,7 @@ def _update_opi_info(opi_key, opi_file_name, descriptive_device_name):
         opi_key: Key to identify to OPI to the GUI
         opi_file_name: File name to the device's opi
         descriptive_device_name: Human readable device name
+        device_macro_name: Name used for macro for creating PV prefix
     """
     logging.info("Adding template information to opi info")
     opi_info_path = path.join(OPI_RESOURCES, "opi_info.xml")
@@ -67,7 +78,7 @@ def _update_opi_info(opi_key, opi_file_name, descriptive_device_name):
     if any(entry.find("key").text == opi_key for entry in opis):
         raise RuntimeWarning("OPI with default name already exists")
 
-    opis.append(_generate_opi_entry(opi_key, opi_file_name, descriptive_device_name))
+    opis.append(_generate_opi_entry(opi_key, opi_file_name, descriptive_device_name, device_macro_name))
     with open(opi_info_path, "w") as f:
         f.write(
             etree.tostring(opi_xml, pretty_print=True, encoding='UTF-8', xml_declaration=True, standalone="yes"))
@@ -84,4 +95,4 @@ def create_opi(device_info):
     copyfile(OPI, device_info.opi_file_path())
     replace_in_file(device_info.opi_file_path(), [("$(DEVICE)", "$({})".format(device_info.ioc_name()))])
 
-    _update_opi_info(device_info.opi_key(), device_info.opi_file_name(), device_info.log_name())
+    _update_opi_info(device_info.opi_key(), device_info.opi_file_name(), device_info.log_name(), device_info.ioc_name())
