a3b901749f33edbf305a90fd0f82809c4d58bae3
aaronlong <aaron.long@stfc.ac.uk>
2019-04-01 10:40:47 +0100

    Added patch to test

--- tests/test_ioc_test_framework_utils.py
+++ tests/test_ioc_test_framework_utils.py
@@ -13,6 +13,7 @@ class IocTestFrameworkTest(unittest.TestCase):
 
     @patch("utils.ioc_test_framework_utils.copy_file")
     @patch('utils.ioc_test_framework_utils.replace_in_file')
+    @patch("utils.gui_utils.open", create=True)
     def test_GIVEN_a_device_WHEN_create_ioc_test_framework_files_THEN_files_created_and_test_files_returned(self, *args):
         expected_file = ['device.py']
         test_framework_files = create_test_framework(self.test_device)
